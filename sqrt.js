de07157f07c2291f025d29ae2b0cf1ee34b3022e
Chandrashekhar Singh Mourya <118621709+Singh-csm@users.noreply.github.com>
2023-02-13 00:59:11 +0530

    sqrt

---
+++ sqrt.js
@@ -0,0 +1,8 @@
+69. Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x))
+};
